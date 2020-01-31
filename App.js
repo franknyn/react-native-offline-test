d1515b7c245956095060e46b383f4fccc94c89c6
Phat Huynh <phat.huynh.nz@gmail.com>
2020-02-01 10:46:01 +1100

    added network quality check

--- App.js
+++ App.js
@@ -25,48 +25,146 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import {useNetInfo} from "@react-native-community/netinfo";
+import NetInfo, {useNetInfo} from "@react-native-community/netinfo";
 
-
+let count = 0;
 const App: () => React$Node = () => {
   const netInfo = useNetInfo();
+
   const [messages, setMessages] = useState([]);
+  const [networkQuality, setNetworkQuality] = useState('Unknown');
 
   const addMessage = (message) => {
     setMessages((messages)=> [message].concat(messages));    
   }
-  const networkRequest = () => {
-    addMessage("fetch('https://google.com')")
-    //console.log("fetch('https://google.com')")
+
+  const checkNetworkQuality = function () {
 
     var request = new XMLHttpRequest();
+    //addMessage('Checking network');
+
+    return new Promise(function (resolve, reject) {
+      var timeout=setTimeout(function(){
+          request.abort();
+          timeout=null;
+          reject('bad');
+      },10000);
+
+      request.onreadystatechange = function () {
+
+        if (request.readyState !== 4) return;
+
+        if(timeout){clearTimeout(timeout);timeout=null;}
+        if (request.status >= 200 && request.status < 300) {
+          resolve('good');
+        } else {
+          reject('bad');
+        }
+
+      };
+
+      request.open('GET', 'https://google.com', true)
+      request.send();
+
+    });
+  };
+
+
+  const networkRequest = (id) => {
+    //console.log("fetch('https://google.com')")
+    addMessage("#"+id + " fetch('https://google.com?q="+id+"')")
+    var request = new XMLHttpRequest();
+    request.timeout = 2000;
+    request.ontimeout = function() {
+        console.error('#'+id+ ' timed out');
+    };
+    request.onprogress = function() {
+      addMessage('#'+ id+ ' onprogress');
+      //do something 
+    };
+
+    request.onloadstart = function() {
+      addMessage('#'+ id+ ' onloadstart');
+    }
+
+    request.onerror = function() {
+      //addMessage('#'+ id+ ' onerror');
+    }
+
+    request.onabort = function() {
+      //addMessage('#'+ id+ ' onabort');
+    }
+    request.onprogress = (e) => {
+        // NetInfo.fetch().then(state => {
+        //   if (!netInfo.isInternetReachable) {
+        //     request.abort();
+        //   }
+        // });
+        return;
+    }
+
     request.onreadystatechange = (e) => {
+      addMessage('#'+ id+ ' readyState='+request.readyState);
       if (request.readyState !== 4) {
+
+        NetInfo.fetch().then(state => {
+          if (!state.isInternetReachable) {
+            setNetworkQuality('Disconnected')
+            request.abort();
+            //addMessage('#'+ id+ ' aborted');
+
+          }else {
+            checkNetworkQuality().then(()=> setNetworkQuality('Good')).catch((error)=> {
+                //bad network
+                setNetworkQuality('Bad')
+                request.abort();
+                addMessage('#'+ id+ ' error - *'+error+'* network');
+            });
+          
+          }
+        });
         return;
       }
 
       if (request.status === 200) {
-        addMessage('success');
-
-        //console.log('success', request.responseText);
+        addMessage('#'+ id +' success');
       } else {
-        addMessage('error');
-        //console.warn('error');
+        addMessage('#'+ id +' error');
       }
     };
 
-    request.open('GET', 'https://google.com');
-    request.send(); 
+    request.open('GET', 'https://google.com?q='+id, true);
+    request.send(null);
+  }
+
+  //for btn : single request
+  const singleRequest = () =>{
+      count++;
+      networkRequest(count);
   }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      networkRequest();
+
+    let interval = setInterval(() => {
+      count++;
+      networkRequest(count);
     }, 10000);
+
+     NetInfo.addEventListener(state => {
+      if (!state.isInternetReachable) { 
+        //when offline abort all network requests in progress so they can reach error soner
+        //OR queue them to recover later
+        //addMessage('Offline');
+
+      } else {      
+        //addMessage('Online');
+        //clearInterval(interval);
+        //recover them here
+      }
+    });
+
     return () => clearInterval(interval);
   }, []);
-
-
 
   return (
     <>
@@ -82,6 +180,11 @@ const App: () => React$Node = () => {
               <Text>Type: {netInfo.type}</Text>
               <Text>Is Connected? {netInfo.isConnected.toString()}</Text>
               <Text>Is Internet Reachable? {netInfo.isInternetReachable.toString()}</Text>
+              <Text>Network Quality: {networkQuality}</Text>
+              {/*<Button
+              title="Start Request"
+                onPress={singleRequest}
+              />*/}
             </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Network Request</Text>
